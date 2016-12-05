6a17470651174e5b6fa94d58423920d7f48ee382
Giovanny Hernández <giovannyh30@gmail.com>
2016-12-04 23:52:25 -0400

    Mejoras, Codigo, Imagenes
    Se mejoro el codigo de: informacion, slide, index.
    ademas se optimizaron imagenes y crearon carpetas

--- core/js/main.js
+++ core/js/main.js
@@ -1,6 +1,25 @@
 (function() {
   (function() {
-    return $(function() {});
+    return $(function() {
+      var $banner, mensaje;
+      $banner = {
+        padre: $('#slider'),
+        hijo: $('#slider').children()
+      };
+      console.log($banner.hijo);
+      mensaje = function() {
+        var $altura;
+        $altura = $banner.hijo.outerHeight();
+        console.log($altura);
+        return $banner.padre.css({
+          'height': $altura + 'px'
+        });
+      };
+      mensaje();
+      return $(window).resize(function() {
+        return mensaje();
+      });
+    });
   })();
 
 }).call(this);
--- heart XH/coffee/main.coffee
+++ heart XH/coffee/main.coffee
@@ -1,5 +1,25 @@
 ( ->
   $ ->
-    # alert "enlazado"
+    $banner = {
+      padre: $('#slider'),
+      hijo: $('#slider').children()
+    }
+    console.log($banner.hijo)
+
+    mensaje = ->
+      $altura = $banner.hijo.outerHeight()
+      console.log($altura)
+
+      $banner.padre.css({
+        'height': $altura + 'px'
+        })
+    # Se llama por Default
+    mensaje()
+
+    # Llamada dinámica
+    $(window).resize( ->
+      # Se Ejecutara cada vez que se redimencione
+      mensaje()
+    )
 
 )()
